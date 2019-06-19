246bab81274552d17e553a5124101c7a299f3efe
unknown <adi120.a@gmail.com>
2019-06-19 02:21:35 +0200

    Start project firstly css

---
+++ src/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header>
+            <nav className='nav_main'>
+                <div className='nav_main__title'>Cosmo<span className='title__span'>s</span></div>
+                <div className='nav_main__menu'>
+                    <button className='nav_main__menu__burger'></button>
+                    <div className='menu'>
+                        <ul className='menu__list'> 
+                            <li className='menu__list__item'>swswsw</li>    
+                            <li className='menu__list__item'>swswsw</li>
+                        </ul>                    
+                    </div>
+                </div>
+            </nav>
+
+        </header>
+
+    );
+}
+
+export default Header;
